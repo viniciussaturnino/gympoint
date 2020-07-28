1eb2e706ade5869879588625178a8c6c7d287fdd
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-28 15:34:31 -0300

    Configured sequelize

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Gym' });
+    return res.json({ message: 'Hello Gym' });
 });
 
 export default routes;
