d445d4a698793c6a70d0591898721f6b83585e0b
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-28 15:06:49 -0300

    Configured eslint and prettier in application

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello Gym' });
-})
+  return res.json({ message: 'Hello Gym' });
+});
 
-module.exports = routes;
+export default routes;
