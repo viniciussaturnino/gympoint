50644a112c3efe76a386e767f6bef88ccf7df872
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-28 16:30:14 -0300

    Connected database with user model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello Gym' });
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Vinicius Saturnino',
+        email: 'vinicius@gmail.com',
+        password_hash: '123456',
+    });
+
+    return res.json(user);
 });
 
 export default routes;
