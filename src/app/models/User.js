f88e8eebc9a41f5383785129e22132d1f866d045
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-07-29 00:45:46 -0300

    Created student table and validated data input

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -8,12 +8,21 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING,
             },
             {
                 sequelize,
             }
         );
+
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 8);
+            }
+        });
+
+        return this;
     }
 
     checkPassword(password) {
---
+++ src/database/migrations/20200729024217-create-students.js
@@ -0,0 +1,45 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('students', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            age: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            weight: {
+                type: Sequelize.DECIMAL,
+                allowNull: false,
+            },
+            height: {
+                type: Sequelize.DECIMAL,
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('students');
+    },
+};
